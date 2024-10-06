9b810f01cb5ce38df87ab5e52f9c1d9705093607
courageousstrawberry <elech2a@gmail.com>
2024-10-05 20:55:58 -0700

    begin creating main

--- 210-lab18.cpp
+++ 210-lab18.cpp
@@ -8,6 +8,18 @@ struct Node {
     Node* next;
 };
 
+void addToTail(Node *head, Node tail);
+
 int main() {
+    int choice = 0;
+
+    cout << "Which linked list method should we use?" << endl;
+    cout << "\t[1] New nodes are added at the head of the linked list." << endl;
+    cout << "\t[2] New nodes are added at the tial of the linked list." << endl;
+    cout << "Choice: ";
+    cin >> choice;
+
+    
+
     return 0;
 }
