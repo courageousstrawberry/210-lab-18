84ae0936c1ffd45f350d17f5858f596144a73ed1
courageousstrawberry <elech2a@gmail.com>
2024-10-05 21:03:04 -0700

    begin addToTail function

--- 210-lab18.cpp
+++ 210-lab18.cpp
@@ -8,10 +8,12 @@ struct Node {
     Node* next;
 };
 
-void addToTail(Node *head, Node tail);
+void addToTail(Node *, Node);
 
 int main() {
     int choice = 0;
+    Node *head = nullptr;
+    Node *current = nullptr;
 
     cout << "Which linked list method should we use?" << endl;
     cout << "\t[1] New nodes are added at the head of the linked list." << endl;
@@ -19,7 +21,22 @@ int main() {
     cout << "Choice: ";
     cin >> choice;
 
-    
+    cout << "Enter review rating 0-5: ";
+    cin >> current->rating;
+    cout << "Enter review comments: ";
+    cin >> current->comment;
+
 
     return 0;
+}
+
+void addToTail(Node *head, Node *add){
+    if (!head){
+        head->rating = add->rating;
+        head->comment = add->comment;
+        head->next = nullptr;
+    }
+    else {
+        
+    }
 }
