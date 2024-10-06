0297a7dd2e2b4b7b97cc4fdbaa3af6a1d403e7d5
courageousstrawberry <elech2a@gmail.com>
2024-10-05 21:35:00 -0700

    add input validation and update main

--- 210-lab18.cpp
+++ 210-lab18.cpp
@@ -12,6 +12,7 @@ void addToTail(Node *&, Node *);
 
 int main() {
     int choice = 0;
+    char repeat = 'Y';
     Node *head = nullptr;
     Node *current = new Node;
 
@@ -20,15 +21,27 @@ int main() {
     cout << "\t[2] New nodes are added at the tail of the linked list." << endl;
     cout << "Choice: ";
     cin >> choice;
+    
+    while (repeat == 'Y') {
+        cout << "Enter review rating 0-5: ";
+        cin >> current->rating;
+        cin.ignore();
+        cout << "Enter review comments: ";
+        getline(cin, current->comment);
+        current->next = nullptr;
 
-    cout << "Enter review rating 0-5: ";
-    cin >> current->rating;
-    cin.ignore();
-    cout << "Enter review comments: ";
-    getline(cin, current->comment);
-    current->next = nullptr;
-
-    addToTail(head, current);
+        if (choice == 2) {
+            addToTail(head, current);
+        }
+        cout << "Enter another review? Y/N: ";
+        cin >> repeat;
+        toupper(repeat);
+        while (repeat != 'Y' || repeat != 'N') {
+            cout << "Error! Please enter Y or N." << endl;
+            cin >> repeat;
+            toupper(repeat);
+        }
+    }
 
     current = head; 
     cout << "Review #1:" << current->rating << ", " << current->comment;
